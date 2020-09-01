2ad00c52beff4bebf5b98d6b556323d7b571ee2c
Devesh Patel <deveshp530@gmail.com>
2020-09-01 15:54:22 -0700

    added user model for signin/signup and password hashing

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,18 @@
+import { CustomError} from './custom-error'
+
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500
+  reason = "Error connecting to databse";
+
+  constructor() {
+    super('error connecting to db');
+
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors(){
+    return [
+      {message: this.reason }
+    ]
+  }
+}
