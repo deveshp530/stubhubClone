c29fa6ec9df56051265162d35073260a80644d86
Devesh Patel <deveshp530@gmail.com>
2020-09-11 16:30:02 -0700

    added signout features to nextjs

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,22 @@
-export default ()=>{
-    return <h1>Landing page</h1>
-}
+import buildClient from "../api/build-client";
+
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    //change to name when it is added to models
+    <h3>Welcome {currentUser.email}</h3>
+  ) : (
+    <h1>Please sign in or signup</h1>
+  );
+};
+
+//getIntitlProps:sending the page with the data already populated
+//from the server
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+
+  return data;
+};
+
+export default LandingPage;
