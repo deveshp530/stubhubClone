d097ecdf3a4f2ca04149cb7a41d97e9b465def87
Devesh Patel <deveshp530@gmail.com>
2020-09-30 15:01:27 -0700

    working on streaming on service for ticket creation

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,16 +1,21 @@
 import nats from "node-nats-streaming";
+import { randomBytes } from "crypto";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 console.clear();
 
-const stan = nats.connect("ticketing", "123", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("listener connected to nats");
 
-  const subscription = stan.subscribe("ticket:created");
-
-  subscription.on("message", (msg) => {
-    console.log("message recieved");
+  stan.on("close", () => {
+    console.log("connection closed");
+    process.exit();
   });
+  new TicketCreatedListener(stan).listen();
 });
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
