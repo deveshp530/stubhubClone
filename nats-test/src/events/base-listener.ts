3a9a7ea8d2886aba79c06d144c491b16318e1654
Devesh Patel <deveshp530@gmail.com>
2020-10-01 15:31:44 -0700

    refactored nats service

--- nats-test/src/events/base-listener.ts
+++ nats-test/src/events/base-listener.ts
@@ -1,15 +1,22 @@
 import { Message, Stan } from "node-nats-streaming";
+import { Subjects } from "./subjects";
 
-export abstract class Listener {
-  abstract subject: string;
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Listener<T extends Event> {
+  abstract subject: T["subject"];
   abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: Message): void;
-  protected ackWait = 5 * 1000;
+  abstract onMessage(data: T["data"], msg: Message): void;
   private client: Stan;
+  protected ackWait = 5 * 1000;
 
   constructor(client: Stan) {
     this.client = client;
   }
+
   subscriptionOptions() {
     return this.client
       .subscriptionOptions()
@@ -25,10 +32,11 @@ export abstract class Listener {
       this.queueGroupName,
       this.subscriptionOptions()
     );
-    subscription.on("message", (msg: Message) => {
-      console.log(`Message received:${this.subject} / ${this.queueGroupName}`);
-      const parsedData = this.parseMessage(msg);
 
+    subscription.on("message", (msg: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+
+      const parsedData = this.parseMessage(msg);
       this.onMessage(parsedData, msg);
     });
   }
